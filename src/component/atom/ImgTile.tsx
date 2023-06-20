c1f936cf0320d35f71ced50364333f8a1458934e
youngmoro <morofujix@keio.jp>
2023-06-20 22:45:26 +0900

    add tile-img files
    add img-tile title

--- src/component/atom/ImgTile.tsx
+++ src/component/atom/ImgTile.tsx
@@ -2,20 +2,42 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { styled } from "styled-components";
 
-const ImgTile = ({ name }: { name: string }) => {
+const ImgTile = ({ name, title }: { name: string; title: string }) => {
   return (
     <Link to={`./${name}`}>
-      <Wrapper>
-        <img src={`/${name}.svg`} />
-      </Wrapper>
+      <ImgBox>
+        <img src={`/${name}-tile.jpg`} />
+      </ImgBox>
+      <Text>{title}</Text>
     </Link>
   );
 };
 
 export default ImgTile;
 
-const Wrapper = styled.div`
+const ImgBox = styled.div`
   width: 300px;
   height: 300px;
-  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 12px;
+
+  img {
+    max-width: 100%;
+    max-height: 100%;
+    width: auto;
+    height: auto;
+  }
+`;
+
+const Text = styled.span`
+  color: white;
+  margin: 0px 4px;
+  font-size: 12px;
+  box-sizing: border-box;
+
+  &:hover {
+    border-bottom: 1px solid white;
+  }
 `;
